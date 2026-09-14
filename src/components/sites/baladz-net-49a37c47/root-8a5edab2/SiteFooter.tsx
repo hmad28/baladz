3426ada116c2dfdb65156c315254ef1f5b29095f
Hammad <email1.hammad@gmail.com>
2026-09-14 16:34:45 +0700

    feat: build baladz footer

---
+++ src/components/sites/baladz-net-49a37c47/root-8a5edab2/SiteFooter.tsx
@@ -0,0 +1,14 @@
+export function SiteFooter() {
+  return (
+    <footer className="w-full bg-baladz-orange px-4 pb-7 pt-6 text-center text-black">
+      <p className="text-[17px] font-extrabold leading-tight">
+        Yayasan Baladz Cerdas Mulia
+      </p>
+      <address className="mt-1 text-[12px] not-italic leading-[1.7]">
+        <p>Jl Pasirlayung Barat 44, Padasuka, Cimenyan</p>
+        <p>Kabupaten Bandung 40191</p>
+        <p>West Java, Indonesia</p>
+      </address>
+    </footer>
+  );
+}
